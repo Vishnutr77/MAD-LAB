008fdb6938be6e94a2b5128832de6b68953a3cb4
Vishnutr77 <vishnutr9961@gmail.com>
2025-09-18 13:18:53 +0530

    Create activityTwo.java

---
+++ Options Menu_9/activityTwo.java
@@ -0,0 +1,14 @@
+package com.example.menunavigation;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class activityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
